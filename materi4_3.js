de84d64749c7e838983c1d3a31605bd2ad1d4f01
persamaanliniersatuvariabel <sitiaisyaah2998@gmail.com>
2021-03-02 15:10:19 +0800

    first commit

--- materi4_3.js
+++ materi4_3.js
@@ -92,7 +92,7 @@ function cekb(){
   }
 }
 function ulangb(){
-  var pil3 = "<td draggable='true' ondragstart='drag(event)' id='p13' class='pilihan'>{1, 2, 3,}</td>";
+  var pil3 = "<td draggable='true' ondragstart='drag(event)' id='p13' class='pilihan'>{1, 2, 3, 4}</td>";
   var pil4 = "<td draggable='true' ondragstart='drag(event)' id='p14' class='pilihan'>{1, 2, 3, 4, 5}</td>";
   var ulang = document.getElementById("ulangb");
   var bahasslh = document.getElementById("bahasslhb");
@@ -103,6 +103,71 @@ function ulangb(){
   ulang.className = ulang.className.replace("tampil", "");
   bahasslh.className = bahasslh.className.replace("tampil", "");
 }
+
+/*soal 3*/
+function cekc(){
+  var jawab = document.getElementById("no3").innerText;
+  var p3 = document.getElementById("p15").innerText;
+  var ulang = document.getElementById("ulangc");
+  var bahasbnr = document.getElementById("bahasbnrc");
+  var bahasslh = document.getElementById("bahasslhc");
+  //cek
+  if(jawab==p3){
+    document.getElementById("iconc").innerHTML = "<img src='benar.jpg' style='width:15px'>";
+    bahasbnr.className += "tampil";
+  } else if(jawab==""){
+    alert("Jawabanmu masih kosong!");
+  } else{
+    ulang.className += "tampil";
+    document.getElementById("iconc").innerHTML = "<img src='salah.png' style='width:15px'>";
+    bahasslh.className += "tampil";
+  }
+}
+function ulangc(){
+  var pil5 = "<td draggable='true' ondragstart='drag(event)' id='p15' class='pilihan'>{1, 2}</td>";
+  var pil6 = "<td draggable='true' ondragstart='drag(event)' id='p16' class='pilihan'>{1, 2, 3}</td>";
+  var ulang = document.getElementById("ulangc");
+  var bahasslh = document.getElementById("bahasslhc");
+  //menghapus semua
+  document.getElementById("no3").innerHTML="";
+  document.getElementById("pil5").innerHTML = pil5 + pil6 ;
+  document.getElementById("iconc").innerHTML = "";
+  ulang.className = ulang.className.replace("tampil", "");
+  bahasslh.className = bahasslh.className.replace("tampil", "");
+}
+
+/*soal 4*/
+function cekd(){
+  var jawab = document.getElementById("no4").innerText;
+  var p4 = document.getElementById("p18").innerText;
+  var ulang = document.getElementById("ulangd");
+  var bahasbnr = document.getElementById("bahasbnrd");
+  var bahasslh = document.getElementById("bahasslhd");
+  //cek
+  if(jawab==p4){
+    document.getElementById("icond").innerHTML = "<img src='benar.jpg' style='width:15px'>";
+    bahasbnr.className += "tampil";
+  } else if(jawab==""){
+    alert("Jawabanmu masih kosong!");
+  } else{
+    ulang.className += "tampil";
+    document.getElementById("icond").innerHTML = "<img src='salah.png' style='width:15px'>";
+    bahasslh.className += "tampil";
+  }
+}
+function ulangd(){
+  var pil7 = "<td draggable='true' ondragstart='drag(event)' id='p15' class='pilihan'>{1, 2}</td>";
+  var pil8 = "<td draggable='true' ondragstart='drag(event)' id='p16' class='pilihan'>{1, 2, 3}</td>";
+  var ulang = document.getElementById("ulangd");
+  var bahasslh = document.getElementById("bahasslhd");
+  //menghapus semua
+  document.getElementById("no4").innerHTML="";
+  document.getElementById("pil8").innerHTML = pil7 + pil8 ;
+  document.getElementById("icond").innerHTML = "";
+  ulang.className = ulang.className.replace("tampil", "");
+  bahasslh.className = bahasslh.className.replace("tampil", "");
+}
+
 
 function info() {
     var popup = document.getElementById("infovar");
@@ -270,7 +335,7 @@ function cek2(){
     bahasslh.className = bahasslh.className.replace("tampil", "");
   }
   
-  //   soal3
+//   soal3
 // latihan
 
  //cek jawaban
@@ -341,6 +406,228 @@ function ulang3(){
   ang6.className = ang6.className.replace("boxmerah","");
   ulang.className = ulang.className.replace("tampil", "");
   document.getElementById("icon3").innerHTML = "";
+  bahasslh.className = bahasslh.className.replace("tampil", "");
+}
+
+//   soal4
+// latihan
+
+ //cek jawaban
+ function cek4(){
+  var var4 = document.getElementById("var4");
+  var ang7 = document.getElementById("ang7");
+  var ang8 = document.getElementById("ang8");
+  var ulang = document.getElementById("ulang4");
+  var bahasbnr = document.getElementById("bahasbnr4");
+  var bahasslh = document.getElementById("bahasslh4");
+
+  //mengecek apakah variabel yang dimasukkan angka
+  var numbers = /^[-+]?[0-9]+$/;
+  if (var4.value.match(numbers)){
+    var x = 0;
+    document.getElementById("ket3").innerHTML = "Variabel di isi dengan huruf. Bukan dengan angka.";
+    document.getElementById("ket3").style.color = "red";
+  }
+
+  //mengecek apakah variabel yang dimasukkan huruf kapital
+  var capital = /^[A-Z]/;
+  if (var4.value.match(capital)){
+    var y = 0;
+    document.getElementById("ket3").innerHTML = "Variabel di isi dengan huruf kecil. Bukan huruf kapital.";
+    document.getElementById("ket3").style.color = "red";
+  }
+
+  //cek
+  if(x!=0 && y!==0 && ang7.value==">" && ang8.value=="3"){
+    document.getElementById("icon4").innerHTML = "<img src='benar.jpg' width='15px'>";
+    bahasbnr.className += "tampil";
+    document.getElementById('ket3').innerHTML = var4.value;
+  } else{
+    ulang.className += "tampil";
+    document.getElementById("icon4").innerHTML = "<img src='salah.png' width='15px'>";
+    bahasslh.className += "tampil";
+  }
+
+  if(var4.value=="" || ang7.value=="" || ang8.value==""){
+    alert("Ups! Jawabanmu masih belum lengkap");
+  }
+
+  //boxmerah
+  if(x==0 || y==0 || var4.value==""){
+      var4.className += " boxmerah";
+    }
+  if(ang7.value!=">"){
+    ang7.className += " boxmerah";
+  }
+  if(ang8.value!="3"){
+    ang8.className += " boxmerah";
+  }
+}
+
+//ulang
+function ulang4(){
+  var var4 = document.getElementById("var4");
+  var ang7 = document.getElementById("ang7");
+  var ang8 = document.getElementById("ang8");
+  var ulang = document.getElementById("ulang4");
+  var bahasslh = document.getElementById("bahasslh4");
+
+  var4.value = "";
+  ang7.value = "";
+  ang8.value = "";
+  var4.className = var4.className.replace("boxmerah","");
+  ang7.className = ang7.className.replace("boxmerah","");
+  ang8.className = ang8.className.replace("boxmerah","");
+  ulang.className = ulang.className.replace("tampil", "");
+  document.getElementById("icon4").innerHTML = "";
+  bahasslh.className = bahasslh.className.replace("tampil", "");
+}
+
+//   soal5
+// latihan
+
+ //cek jawaban
+ function cek5(){
+  var var5 = document.getElementById("var5");
+  var ang9 = document.getElementById("ang9");
+  var ang10 = document.getElementById("ang10");
+  var ulang = document.getElementById("ulang5");
+  var bahasbnr = document.getElementById("bahasbnr5");
+  var bahasslh = document.getElementById("bahasslh5");
+
+  //mengecek apakah variabel yang dimasukkan angka
+  var numbers = /^[-+]?[0-9]+$/;
+  if (var5.value.match(numbers)){
+    var x = 0;
+    document.getElementById("ket4").innerHTML = "Variabel di isi dengan huruf. Bukan dengan angka.";
+    document.getElementById("ket4").style.color = "red";
+  }
+
+  //mengecek apakah variabel yang dimasukkan huruf kapital
+  var capital = /^[A-Z]/;
+  if (var5.value.match(capital)){
+    var y = 0;
+    document.getElementById("ket4").innerHTML = "Variabel di isi dengan huruf kecil. Bukan huruf kapital.";
+    document.getElementById("ket4").style.color = "red";
+  }
+
+  //cek
+  if(x!=0 && y!==0 && ang9.value==">" && ang10.value=="-4"){
+    document.getElementById("icon5").innerHTML = "<img src='benar.jpg' width='15px'>";
+    bahasbnr.className += "tampil";
+    document.getElementById('ket4').innerHTML = var5.value;
+  } else{
+    ulang.className += "tampil";
+    document.getElementById("icon5").innerHTML = "<img src='salah.png' width='15px'>";
+    bahasslh.className += "tampil";
+  }
+
+  if(var5.value=="" || ang9.value=="" || ang10.value==""){
+    alert("Ups! Jawabanmu masih belum lengkap");
+  }
+
+  //boxmerah
+  if(x==0 || y==0 || var5.value==""){
+      var5.className += " boxmerah";
+    }
+  if(ang9.value!=">"){
+    ang9.className += " boxmerah";
+  }
+  if(ang10.value!="-4"){
+    ang10.className += " boxmerah";
+  }
+}
+
+//ulang
+function ulang5(){
+  var var5 = document.getElementById("var5");
+  var ang9 = document.getElementById("ang9");
+  var ang10 = document.getElementById("ang10");
+  var ulang = document.getElementById("ulang5");
+  var bahasslh = document.getElementById("bahasslh5");
+
+  var5.value = "";
+  ang9.value = "";
+  ang10.value = "";
+  var5.className = var5.className.replace("boxmerah","");
+  ang9.className = ang9.className.replace("boxmerah","");
+  ang10.className = ang10.className.replace("boxmerah","");
+  ulang.className = ulang.className.replace("tampil", "");
+  document.getElementById("icon5").innerHTML = "";
+  bahasslh.className = bahasslh.className.replace("tampil", "");
+}
+
+//   soal6
+// latihan
+
+ //cek jawaban
+ function cek6(){
+  var var6 = document.getElementById("var6");
+  var ang11 = document.getElementById("ang11");
+  var ang12 = document.getElementById("ang12");
+  var ulang = document.getElementById("ulang6");
+  var bahasbnr = document.getElementById("bahasbnr6");
+  var bahasslh = document.getElementById("bahasslh6");
+
+  //mengecek apakah variabel yang dimasukkan angka
+  var numbers = /^[-+]?[0-9]+$/;
+  if (var6.value.match(numbers)){
+    var x = 0;
+    document.getElementById("ket5").innerHTML = "Variabel di isi dengan huruf. Bukan dengan angka.";
+    document.getElementById("ket5").style.color = "red";
+  }
+
+  //mengecek apakah variabel yang dimasukkan huruf kapital
+  var capital = /^[A-Z]/;
+  if (var6.value.match(capital)){
+    var y = 0;
+    document.getElementById("ket5").innerHTML = "Variabel di isi dengan huruf kecil. Bukan huruf kapital.";
+    document.getElementById("ket5").style.color = "red";
+  }
+
+  //cek
+  if(x!=0 && y!==0 && ang11.value=="<" && ang12.value=="-0,5"){
+    document.getElementById("icon6").innerHTML = "<img src='benar.jpg' width='15px'>";
+    bahasbnr.className += "tampil";
+    document.getElementById('ket5').innerHTML = var6.value;
+  } else{
+    ulang.className += "tampil";
+    document.getElementById("icon6").innerHTML = "<img src='salah.png' width='15px'>";
+    bahasslh.className += "tampil";
+  }
+
+  if(var6.value=="" || ang11.value=="" || ang12.value==""){
+    alert("Ups! Jawabanmu masih belum lengkap");
+  }
+
+  //boxmerah
+  if(x==0 || y==0 || var6.value==""){
+      var6.className += " boxmerah";
+    }
+  if(ang11.value!="<"){
+    ang11.className += " boxmerah";
+  }
+  if(ang12.value!="-0,5"){
+    ang12.className += " boxmerah";
+  }
+}
+
+//ulang
+function ulang6(){
+  var var6 = document.getElementById("var6");
+  var ang11 = document.getElementById("ang11");
+  var ang12 = document.getElementById("ang12");
+  var ulang = document.getElementById("ulang6");
+  var bahasslh = document.getElementById("bahasslh6");
+
+  var6.value = "";
+  ang11.value = "";
+  ang12.value = "";
+  var6.className = var6.className.replace("boxmerah","");
+  ang11.className = ang11.className.replace("boxmerah","");
+  ang12.className = ang12.className.replace("boxmerah","");
+  ulang.className = ulang.className.replace("tampil", "");
+  document.getElementById("icon6").innerHTML = "";
   bahasslh.className = bahasslh.className.replace("tampil", "");
 }
 
